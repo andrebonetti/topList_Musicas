6c7ac72fd316cd7d13bd4cb0ee959b3bb396c2de
andrebonetti <andrebonetti2@gmail.com>
2016-02-23 00:54:10 -0300

    Mudança Sistema Votação

--- application/controllers/votacao.php
+++ application/controllers/votacao.php
@@ -4,60 +4,18 @@
         
 		public function index(){
             
-        $todas_musicas  =  $this->crud_model->get_desc("Id");  
-        $vertentes      =  $this->crud_model->get_vertentes();    
-        $todas_votos    =  $this->crud_model->get_votos();
-            
-        $hasCombate = false;
-        $contagem = 0;    
-        foreach($todas_musicas as $musica) 
-        {
-            if($musica["Id"] != count($todas_musicas))
-            {
-               $contagem = $contagem +  $musica["Id"];
-            }
-        }
-            
-            if(count($todas_votos) < $contagem){
-                $hasCombate = true;
-            }
+        $nao_votados = $this->crud_model->nao_votados(); 
+        $ja_votados = $this->crud_model->ja_votados(); 
+        $total_combates = $this->crud_model->total_combates();    
         
-        if($hasCombate == true){            
-            $musica1 =  $this->crud_model->get_musica1();     
-            $musica2 =  $this->crud_model->get_musica2($musica1["Id"]); 
+        if(count($nao_votados) > 0){
+            $musica1 =  $this->crud_model->get_where_arquivo($nao_votados[0]["musica1"]);
+            $musica2 =  $this->crud_model->get_where_arquivo($nao_votados[0]["musica2"]);
             
-            $validacao              = 0;
-            $valida_votacao1        = $this->crud_model->valida_votacao1($musica1["Arquivo"],$musica2["Arquivo"]);    
-            $valida_votacao2        = $this->crud_model->valida_votacao2($musica1["Arquivo"],$musica2["Arquivo"]); 
             $lista_vitoriasMusica1  = $this->crud_model->lista_vitorias($musica1["Arquivo"]); 
             $lista_vitoriasMusica2  = $this->crud_model->lista_vitorias($musica2["Arquivo"]);
             $lista_derrotasMusica1  = $this->crud_model->lista_derrotas($musica1["Arquivo"]); 
             $lista_derrotasMusica2  = $this->crud_model->lista_derrotas($musica2["Arquivo"]);
-       
-            if(( (empty($valida_votacao1)) && (empty($valida_votacao2)) )){       
-                $validacao = 1;                            
-            }
-            else{    
-                while( (!empty($valida_votacao1)) or (!empty($valida_votacao2)) )
-                {
-                    $musica1 =  $this->crud_model->get_musica1();     
-                    $musica2 =  $this->crud_model->get_musica2($musica1["Id"]); 
-                 
-                    $valida_votacao1 = $this->crud_model->valida_votacao1($musica1["Arquivo"],$musica2["Arquivo"]);    
-                    $valida_votacao2 = $this->crud_model->valida_votacao2($musica1["Arquivo"],$musica2["Arquivo"]); 
-
-                    if(( (empty($valida_votacao1)) && (empty($valida_votacao2)) )){                        
-                        $validacao = 1;  
-                        
-                        $lista_vitoriasMusica1  = $this->crud_model->lista_vitorias($musica1["Arquivo"]); 
-                        $lista_vitoriasMusica2  = $this->crud_model->lista_vitorias($musica2["Arquivo"]);
-                        $lista_derrotasMusica1  = $this->crud_model->lista_derrotas($musica1["Arquivo"]); 
-                        $lista_derrotasMusica2  = $this->crud_model->lista_derrotas($musica2["Arquivo"]);
-                    }
-                    
-                    if($validacao == 1){break;}
-                 }
-            }
             
             $destaques_vitoria1 = array();
             
@@ -98,35 +56,30 @@
             if((count($destaques_vitoria1) > 0)&&(count($destaques_vitoria2) > 0)){
                 echo "Erro Votacao";
             }
-
-            if($validacao == 1){ 
-                
-                /*--------------------------CONTENT----------------------------------*/
-                $content = array("atual_page"       => "votacao"
+            
+            /*--------------------------CONTENT----------------------------------*/
+            $content = array("atual_page"       => "votacao"
                                 ,"musica1"          => $musica1
                                 ,"musica2"          => $musica2
-                                ,"ja_votados"       => count($todas_votos)
-                                ,"total_combates"   => $contagem
-                                ,"vertentes"        => $vertentes
+                                ,"ja_votados"       => count($ja_votados)
+                                ,"nao_votados"      => count($nao_votados)
+                                ,"total_combates"   => count($total_combates)
+                                ,"vitorias_musica1" => $lista_vitoriasMusica1
+                                ,"vitorias_musica2" => $lista_vitoriasMusica2 
                                 ,"derrotas_musica1" => $lista_derrotasMusica1
                                 ,"derrotas_musica2" => $lista_derrotasMusica2
-                                ,"vitorias_musica1" => $lista_vitoriasMusica1
-                                ,"vitorias_musica2" => $lista_vitoriasMusica2
                                 ,"destaques_vitoria1" => $destaques_vitoria1
                                 ,"destaques_vitoria2" => $destaques_vitoria2);
 
-                /*VIEW*/$this->load->template("votacao.php",$content);
-
-            }
-        }
-        else{
-            redirect("");
-        }
-        
+            /*VIEW*/$this->load->template("votacao.php",$content);
             
+        }   
+        
 	   }
         
         public function votar($Id1,$Id2,$isAutomatico = 0){
+            
+            $this->output->enable_profiler(true);
             
             $musica_vencedora = $this->crud_model->get_where($Id1);   
             $musica_derrotada = $this->crud_model->get_where($Id2); 
@@ -142,6 +95,9 @@
             
             $data2["NumeroCombates"] = $contagem2;
             $this->crud_model->update($Id2,$data2);
+            
+            
+            /* ----- INSERE COMBATE ----- */
             
             $data = date('Y-m-d'); 
             $hora = localtime();
@@ -164,16 +120,61 @@
             }
             else{$segundo = $hora[0];}
             
+            $busca_Id1 = $this->crud_model->buscaId_votacao1($musica_vencedora["Arquivo"],$musica_derrotada["Arquivo"]); 
+            var_dump($busca_Id1);
+            if(!(empty($busca_Id1))){
+                $IdVotacao = $busca_Id1["Id"];
+            }
+            else{
+                $busca_Id2 = $this->crud_model->buscaId_votacao1($musica_vencedora["Arquivo"],$musica_derrotada["Arquivo"]); 
+                var_dump($busca_Id2);
+                $IdVotacao = $busca_Id2["Id"];
+            }
+
             $dataEhora = $data." ".$horaA.":".$minuto.":".$segundo;
-            
             $votadas["Musica1"] = $musica_vencedora["Arquivo"];
             $votadas["Musica2"] = $musica_derrotada["Arquivo"];
+            $votadas["IsVotado"] = 1;
             $votadas["Data"] = $dataEhora;
             $votadas["IsAutomatico"] = $isAutomatico;
             
-            $this->crud_model->insert($votadas);
+            $this->crud_model->update_votacao($IdVotacao,$votadas);
             
             redirect(base_url("index.php/votacao/index"));
         }
         
+        public function crate_combate($id = 0){
+            
+            $this->output->enable_profiler(true);
+        
+            for($i=6;$i<=10;$i++){
+                
+                $musica1   =   $this->crud_model->get_where($i);
+
+                $musicas =  $this->crud_model->get_desc("Pontuacao");
+
+                foreach($musicas as $musica2){
+
+                    if($musica1["Arquivo"] != $musica2["Arquivo"])
+                    {
+                        $valida_votacao1 = $this->crud_model->valida_Combate1($musica1["Arquivo"],$musica2["Arquivo"]);    
+                        $valida_votacao2 = $this->crud_model->valida_Combate2($musica1["Arquivo"],$musica2["Arquivo"]); 
+
+                        if(( (empty($valida_votacao1)) && (empty($valida_votacao2)) )){ 
+
+                            $combate["Musica1"] = $musica1["Arquivo"];
+                            $combate["Musica2"] = $musica2["Arquivo"];
+                            $combate["IsVotado"] = 0;
+                            $combate["Data"] = '0';
+                            $combate["IsAutomatico"] = 0;
+
+                            $this->crud_model->insert($combate);
+                        }
+                    }
+                }
+                
+            }
+            
+            echo "pronto";
+        }
     }
--- application/models/crud_model.php
+++ application/models/crud_model.php
@@ -29,6 +29,12 @@
 			return $this->db->get("lista_musicas")->row_array();	
 		}
         
+        function get_where_arquivo($Arquivo){
+			$this->db->where("Arquivo",$Arquivo);
+            $this->db->join('vertentes', 'vertentes.IdVertente = lista_musicas.Vertente');
+			return $this->db->get("lista_musicas")->row_array();	
+		}
+        
         /*CRIA VOTACAO*/
         function get_musica1(){
             $this->db->join('vertentes', 'vertentes.IdVertente = lista_musicas.Vertente');
@@ -47,6 +53,7 @@
         function valida_votacao1($musica1,$musica2){
             $this->db->where("musica1",$musica1);
             $this->db->where("musica2",$musica2);
+            $this->db->where("IsVotado",1);
 			return $this->db->get("votacao")->result_array();	
 		}
         
@@ -54,10 +61,37 @@
         function valida_votacao2($musica1,$musica2){
             $this->db->where("musica1",$musica2);
             $this->db->where("musica2",$musica1);
+            $this->db->where("IsVotado",1);
 			return $this->db->get("votacao")->result_array();	
 		}
         
+        function buscaId_votacao1($musica1,$musica2){
+            $this->db->where("musica1",$musica1);
+            $this->db->where("musica2",$musica2);
+            $this->db->where("IsVotado",0);
+			return $this->db->get("votacao")->row_array();	
+		}
+        
+        function buscaId_votacao2($musica1,$musica2){
+            $this->db->where("musica1",$musica2);
+            $this->db->where("musica2",$musica1);
+            $this->db->where("IsVotado",0);
+			return $this->db->get("votacao")->row_array();	
+		}
+        
+        function valida_Combate1($musica1,$musica2){
+            $this->db->where("musica1",$musica1);
+            $this->db->where("musica2",$musica2);
+			return $this->db->get("votacao")->row_array();	
+		}
+        function valida_Combate2($musica1,$musica2){
+            $this->db->where("musica1",$musica2);
+            $this->db->where("musica2",$musica1);
+			return $this->db->get("votacao")->row_array();	
+		}
+        
         function valida_votacao3($musica1){
+            $this->db->where("IsVotado",1);
             $this->db->where("musica1",$musica1);
             $this->db->or_where("musica2",$musica1);
 			return $this->db->get("votacao")->result_array();	
@@ -65,13 +99,31 @@
         
         function lista_derrotas($musica){
             $this->db->where("musica2",$musica);
+            $this->db->where("IsVotado",1);
 			return $this->db->get("votacao")->result_array();	
 		}
         
         function lista_vitorias($musica){
             $this->db->where("musica1",$musica);
+            $this->db->where("IsVotado",1);
 			return $this->db->get("votacao")->result_array();	
-		}      
+		} 
+        
+        function nao_votados(){
+            $this->db->where("IsVotado",0);
+            $this->db->order_by("Id","random");
+			return $this->db->get("votacao")->result_array();	
+		} 
+        
+        function ja_votados(){
+            $this->db->where("IsVotado",1);
+            $this->db->order_by("Id","random");
+			return $this->db->get("votacao")->result_array();	
+		}  
+        
+        function total_combates(){
+			return $this->db->get("votacao")->result_array();	
+		}  
 		
 		/* INSERT */
 		function insert($data){
@@ -82,6 +134,11 @@
 		function update($Id,$data){
 			$this->db->where 	('Id', $Id);
 			$this->db->update	("lista_musicas",$data);
+		}
+        
+        function update_votacao($Id,$data){
+			$this->db->where 	('Id', $Id);
+			$this->db->update	("votacao",$data);
 		}
 		
 		/* DELETE */
